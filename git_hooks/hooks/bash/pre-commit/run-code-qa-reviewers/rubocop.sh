032b527c5f436070564b69bdf8ac96a59606575e
Rubén González <ruben.gonzalez.martinez@gmail.com>
2018-01-31 23:13:38 +0100

    Improve Rubocop pre-commit hook
    First tests if Rubocop is installed
    Then, we print Rubocop output message and use Rubocop exit codes

--- git_hooks/hooks/bash/pre-commit/run-code-qa-reviewers/rubocop.sh
+++ git_hooks/hooks/bash/pre-commit/run-code-qa-reviewers/rubocop.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-rubocop "$@"
-case "$?" in
-	0 ) 	echo "[Rubocop]: Your code is clean"; 											exit 0;;
-  1 ) 	echo "[Rubocop]: Your commit is a quite dirty"; 						exit 1;;
-	2 ) 	echo "[Rubocop]: Invalid options to this command"; 					exit 2;;
-  127 ) echo "[Rubocop]: You don't have this command installed"; 		exit 0;;
-	* ) 	echo "[Rubocop]: There is something strange at this point"; exit 1;;
-esac
+
+# Check if rubocop is installed for the current project
+bin/bundle exec rubocop -v >/dev/null 2>&1 || { echo >&2 "[Rubocop]: Add rubocop to your Gemfile"; exit 1; }
+
+# Executes rubocop as The-Cocktail tools guidestyle
+bin/bundle exec rubocop -a -D -S
